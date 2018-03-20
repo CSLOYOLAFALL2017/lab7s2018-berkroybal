23a60abb57bd7e1f5164793bf16633e8ab164985
alexa <amfernandez@loyola.edu>
2018-03-20 10:40:46 -0400

    version0

--- lab7BerkFernandezRoybal.py
+++ lab7BerkFernandezRoybal.py
@@ -1,1 +1,23 @@
-def filename: 
+def file_name_input():
+    file_name = input("Enter a file name > ")
+    fileNotFound = True  # get inside loop at least once
+    while (fileNotFound):
+        try:
+            movie_file = open(file_name, "r")
+            fileNotFound = False  # do not get back in loop
+            # read from file
+        except FileNotFoundError:  # ask for file name again
+            file_name = input("Enter a file name >")
+    return file_name
+
+def max(file_name):
+    file_name = file_name_input()
+    movie_file = open(file_name, "r")
+    max_profit = 0
+    for line in file_name:
+        movie_info = "release_date, title, budget, boxoffice_gross"
+        release_date, title, budget, boxoffice_gross = movie_info.split(",")
+        profit = boxoffice_gross - budget
+        if profit > max_profit:
+            max_profit = profit
+    return max_profit
